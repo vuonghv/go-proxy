66a237e79b645261a6a28fc742e9f2e19c2e45f5
Vuong Hoang <vuonghv.cs@gmail.com>
2022-11-14 23:19:20 +0700

    Impl basic-reverse-proxy

--- README.md
+++ README.md
@@ -2,6 +2,24 @@
 
 ## Reverse Proxy
 
+1. Run HTTP server at port 8080
+    ```
+    go run http-server.go
+    ```
+
+2. Run reverse proxy at port 9090
+    ```
+    go run basic-reverse-proxy.go
+    ```
+
+3. Testing
+    ```
+    curl http://localhost:9090/abc/xyz
+    Hello /abc/xyz [127.0.0.1:55561]
+    ```
+
 ## Forward Proxy
+
+TODO
 
 ## Happy Coding!
---
+++ basic-reverse-proxy.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "flag"
+    "log"
+    "strings"
+    "net/url"
+    "net/http"
+    "net/http/httputil"
+)
+
+func main() {
+    fromAddr := flag.String("from", "127.0.0.1:9090", "proxy's listening address")
+    toAddr := flag.String("to", "127.0.0.1:8080", "the address this proxy will forward to")
+    flag.Parse()
+
+    toUrl := parseToUrl(*toAddr)
+    proxy := httputil.NewSingleHostReverseProxy(toUrl)
+    log.Println("Starting proxy server on", *fromAddr)
+
+    if err := http.ListenAndServe(*fromAddr, proxy); err != nil {
+        log.Fatal("ListenAndServe:", err)
+    }
+}
+
+// parseToUrl parses a "to" address to url.URL value
+func parseToUrl(addr string) *url.URL {
+    if !strings.HasPrefix(addr, "http") {
+        addr = "http://" + addr
+    }
+
+    toUrl, err := url.Parse(addr)
+    if err != nil {
+        log.Fatal("parseToUrl:", err)
+    }
+
+    return toUrl
+}
